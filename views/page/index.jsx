34e60e25e20c3eb043558e11bfc80ab0f7da2cfe
emmmmmmmm <8318670+wx_d05cd8ec6e@user.noreply.gitee.com>
2024-08-09 20:37:26 +0800

    12

--- views/page/index.jsx
+++ views/page/index.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, useRoutes} from "react-router-dom";
 import routesConfig from "./routes";
 
 // 注释这个方法，就能编译成功
+/*
 function getRoutes(routes){
     return routes.map(({...route}) => {
         if (typeof route.element ==='string')
@@ -10,7 +11,7 @@ function getRoutes(routes){
             const path = route.element;
             const Component=React.lazy(() => import(`@/page/${path}`));
             delete route.element;
-            const result={...route, element:<Suspense fallback={<Loading/>}><Component/></Suspense>};
+            const result={...route, element:<Suspense fallback={<div>loading...</div>}><Component/></Suspense>};
             const children=result.children;
             if (children){
                 result.children=getRoutes(children);
@@ -22,6 +23,7 @@ function getRoutes(routes){
 
     });
 }
+*/
 
 
 export default ()=>{
